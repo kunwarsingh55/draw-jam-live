1581743751731c614ac02be118f8290d8b8d08d4
kunwarsingh55 <kps20k@gmail.com>
2024-07-16 19:19:07 +0530

    Fix CORS

--- server/index.js
+++ server/index.js
@@ -15,7 +15,13 @@ const io = new Server(server, {
 });
 
 // Middleware
-app.use(cors());
+app.use(cors(
+    {
+        "origin" : "https://draw-jam-frontend.vercel.app/",
+        "methods" : ["POST", "GET", "PUT"],
+        "credentials" : true
+    }
+));
 app.use(express.json());
 app.use('/auth', authRoutes);
 app.use('/api', apiRoutes);
